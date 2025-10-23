2a148843ce71da16bf34586f228fcaf4c7d5fed0
huttatran <hdtran114@student.hbuhsd.edu>
2025-10-23 21:35:40 +0000

    fim

--- Main.java
+++ Main.java
@@ -4,6 +4,63 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a word: ");
+    String word = sc.nextLine();
+    String maxL = "";
+    String currentLetter = "";
+    int count = 0;
+    int maxCount = count;
+    
+    for (int i = 0; i < word.length(); i++)
+    {
+
+        /*********** This here is the "regular" counting algorithm *****/
+        String target = word.substring(i, i+1);
+        count = 0;
+        for (int j = 0; j < word.length(); j++)
+        {
+            currentLetter = word.substring(j, j+1);
+            if (currentLetter.equals(target))
+            {
+                count++;
+            }
+            
+        }
+        if (maxCount < count) {
+              maxCount = count;
+              maxL = target;
+            }
+        /***************************************************************/
+
+    }
+    System.out.println("The letter " + maxL + " appears the most times.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    sc.close();
   }
 }
